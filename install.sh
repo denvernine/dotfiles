6421244a26faffe9910231c4e84f095ab5f84381
denvernine <37843385+denvernine@users.noreply.github.com>
2021-06-12 17:27:57 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,26 +5,27 @@
 read -p 'This may overwrite existing files in your home directory. Are you sure? (y/n): ';
 
 if [[ ${REPLY} =~ ^[Yy]$ ]]; then
-  if type curl &> /dev/null || type wget &> /dev/null; then
-    dotfiles_directory="${HOME}/dotfiles"
-    tarball_url='https://github.com/denvernine/dotfiles/archive/master.tar.gz'
+  read -p "install directory [${HOME}/denvernine/dotfiles]: ";
 
+  if type curl &> /dev/null || type wget &> /dev/null; then
+    dotfiles_directory="${REPLY:=${HOME}/denvernine/dotfiles}"
+    tarball_url='https://github.com/denvernine/dotfiles/archive/master.tar.gz'
+    
     if type curl &> /dev/null; then
       curl -L "${tarball_url}"
     elif type wget &> /dev/null; then
       wget -O - "${tarball_url}"
     fi | tar zxv
-
+   
+    unset tarball_url
+    
     mv -f dotfiles-master "${dotfiles_directory}"
+   
+    . "${dotfiles_directory}/deploy.sh"
   else
     echo 'curl or wget required.'
     exit 1
   fi
-
-  . "${dotfiles_directory}/deploy.sh"
 else
   echo 'abort.'
 fi
-
-unset dotfiles_directory
-unset tarball_url
